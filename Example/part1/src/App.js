08d1fc2e1143135b8e63e041a9498e7307dd30fe
Juan J. Garcia <juanjgarcialeon@gmail.com>
2023-01-21 03:52:28 -0500

    Increase and decrease buttons

--- Example/part1/src/App.js
+++ Example/part1/src/App.js
@@ -1,15 +1,34 @@
 import React, { useState } from "react";
 
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+
 const App = () => {
 	const [counter, setCounter] = useState(0)
 
-	setTimeout(
-		() => setCounter(counter + 1), 1000
-	)
+	// setTimeout(
+	// 	() => setCounter(counter + 1), 1000
+	// )
 
+	const handleClick = (num) => setCounter(counter + num)
+	const setToZero = () => setCounter(0)
+	
 	return (
 		<div>
-			{counter}
+			<Display counter={counter} />
+			<Button 
+				handleClick={() => handleClick(1)} 
+				text={'plus'} />
+			<Button 
+				handleClick={() => handleClick(-counter)}
+				text={'zero'} />
+			<Button 
+				handleClick={setToZero}
+				text={'zero 2'} />
+			<Button 
+				handleClick={() => handleClick(-1)}
+				text={'minus'} />
 		</div>
 	);
 }
