3395a0bc38f12268fcd603810e18c401c70db8bc
Juan J. Garcia <juanjgarcialeon@gmail.com>
2023-01-22 01:44:51 -0500

    Attempt to handle both buttons with one function

--- Example/part1/src/App2.js
+++ Example/part1/src/App2.js
@@ -1,25 +1,41 @@
 import { useState } from "react"
 
+
 const App = () => {
 	const [click, setClick] = useState({
 		left: 0, right: 0
 	})
 
+	const handleClick = (button) => {
+		const otherButton = (button === 'right') ? 'left' : 'right'
+		const newClick = {
+			[button]: click[button] + 1,
+			[otherButton]: click[otherButton]
+		}
+		setClick(newClick)
+	}
+
 	return (
 		<div>
 			{click.left}
-			<button onClick={
+			{/* <button onClick={
 				() => setClick({
 					left: click.left + 1, right: click.right
 				})
 			}>
 				left
+			</button> */}
+			<button onClick={() => handleClick('left')}>
+				left
 			</button>
-			<button onClick={
+			{/* <button onClick={
 				() => setClick({
 					left: click.left, right: click.right + 1}
 				)
 			}>
+				right
+			</button> */}
+			<button onClick={() => handleClick('right')}>
 				right
 			</button>
 			{click.right}
