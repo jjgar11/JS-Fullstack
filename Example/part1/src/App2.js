2d67a61c5f52e9f082d9b6f101a0c52b0ae682c5
Juan J. Garcia <juanjgarcialeon@gmail.com>
2023-01-22 01:18:42 -0500

    Use of just one useState for two changing objects. (Save and object inside the useState variable)

--- Example/part1/src/App2.js
+++ Example/part1/src/App2.js
@@ -1,19 +1,28 @@
 import { useState } from "react"
 
 const App = () => {
-	const [left, setLeft] = useState(0)
-	const [right, setRight] = useState(0)
+	const [click, setClick] = useState({
+		left: 0, right: 0
+	})
 
 	return (
 		<div>
-			{left}
-			<button onClick={() => setLeft(left + 1)}>
+			{click.left}
+			<button onClick={
+				() => setClick({
+					left: click.left + 1, right: click.right
+				})
+			}>
 				left
 			</button>
-			<button onClick={() => setRight(right + 1)}>
+			<button onClick={
+				() => setClick({
+					left: click.left, right: click.right + 1}
+				)
+			}>
 				right
 			</button>
-			{right}
+			{click.right}
 		</div>
 	)
 }
