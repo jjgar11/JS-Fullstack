06468d37e844543c5b8c10a1c865d7c96fe64df6
Juan J. Garcia <juanjgarcialeon@gmail.com>
2023-01-22 04:11:34 -0500

    History and Button components with conditional rendering. Addition of center button to try the button component

--- Example/part1/src/App2.js
+++ Example/part1/src/App2.js
@@ -1,21 +1,45 @@
 import { useState } from "react"
 
 
+const History = ({ allClicks }) => {
+	if (allClicks.length === 0) {
+		return (
+			<div>The app is used by pressing the buttons</div>
+		)
+	}
+	return (
+		<div>button press history: {allClicks.join(' ')}</div>
+	)
+}
+
+const Button = ({ handleButton, side }) => {
+	return(
+		<button onClick={() => handleButton(side)}>
+			{side}
+		</button>
+	)
+}
+
 const App = () => {
 	const [click, setClick] = useState({
-		left: 0, right: 0
+		left: 0, right: 0, center: 0
 	})
+	const [allClicks, setAll] = useState([])
 
-	const handleClick = (button) => setClick({...click, [button]: click[button] + 1})
+	const handleClick = (button) => {
+		setClick({...click, [button]: click[button] + 1})
+		setAll(allClicks.concat(button[0].toUpperCase()))
+	}
 
 	return (
 		<div>
 			{click.left}
-			<button onClick={() => handleClick('left')}>
-				left</button>
-			<button onClick={() => handleClick('right')}>
-				right</button>
+			<Button handleButton={handleClick} side='left' />
+			<Button handleButton={handleClick} side='center' />
+			{click.center}
+			<Button handleButton={handleClick} side='right' />
 			{click.right}
+			<History allClicks={allClicks} />
 		</div>
 	)
 }
