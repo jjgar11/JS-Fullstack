eb3b20d19ca003b1a7bdec6195022d1769efcfee
Juan J. Garcia <juanjgarcialeon@gmail.com>
2023-01-23 16:54:07 -0500

    Another way to use event handlers

--- Example/part1/src/App2.js
+++ Example/part1/src/App2.js
@@ -20,11 +20,20 @@ const Button = ({ handleButton, side }) => {
 	)
 }
 
+const Button2 = ({ handleButton, text }) => {
+	return(
+		<button onClick={handleButton}>
+			{text}
+		</button>
+	)
+}
+
 const App = () => {
 	const [click, setClick] = useState({
 		left: 0, right: 0, center: 0
 	})
 	const [allClicks, setAll] = useState([])
+	const [value, setValue] = useState(10)
 
 	const handleClick = (button) => () => {
 		setClick({...click, [button]: click[button] + 1})
@@ -32,6 +41,7 @@ const App = () => {
 	}
 
 	return (
+		<>
 		<div>
 			{click.left}
 			<Button handleButton={handleClick} side='left' />
@@ -41,6 +51,13 @@ const App = () => {
 			{click.right}
 			<History allClicks={allClicks} />
 		</div>
+		<div>
+			{value}
+			<Button2 handleButton={() => setValue(1000)} text={'thousand'} />
+			<Button2 handleButton={() => setValue(0)} text={'reset'} />
+			<Button2 handleButton={() => setValue(value+1)} text={'increment'} />
+		</div>
+		</>
 	)
 }
 
