e65a83eb795d036e5b8abf4b283252eeb1997d74
Juan J. Garcia <juanjgarcialeon@gmail.com>
2023-01-23 19:40:27 -0500

    Exercise part1 - d

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,72 @@
+import React, {useState} from 'react';
+import ReactDOM from 'react-dom/client';
+
+const Button = ({ handleClick, qualification, text }) => {
+	return(
+		<button onClick={() => handleClick(qualification + 1)}>
+			{text}
+		</button>
+	)
+}
+
+const Statistics = ({ stats }) => {
+	const [good, neutral, bad] = stats
+	const total = good+neutral+bad
+	const average = total === 0 ? 0 : (good-bad)/total
+	const pos = total === 0 ? 0 : good/total*100
+
+	if (total === 0) {
+		return(
+			<h3>No feedback given</h3>
+		)
+	}
+	return(
+		<div>
+			<h2>Statistics</h2>
+			<table>
+			<tbody>
+				<Statistic text='Good' value={good} />
+				<Statistic text='Neutral' value={neutral} />
+				<Statistic text='Bad' value={bad} />
+				<Statistic text='All' value={total} />
+				<Statistic text='Average' value={average} />
+				<Statistic text='Positive' value={pos + '%'} />
+			</tbody>
+			</table>
+		</div>
+	)
+}
+
+const Statistic = ({ text, value }) => {
+	return <tr>
+			<td>{text}</td>
+			<td>{value}</td>
+		</tr>
+}
+
+const App = () => {
+	const [good, setGood] = useState(0)
+	const [neutral, setNeutral] = useState(0)
+	const [bad, setBad] = useState(0)
+
+	return (
+		<>
+		<div>
+			<h1>Give feedback</h1>
+		</div>
+		<div>
+			<Button handleClick={setGood} qualification={good} text='good' />
+			<Button handleClick={setNeutral} qualification={neutral} text='neutral' />
+			<Button handleClick={setBad} qualification={bad} text='bad' />
+		</div>
+		<Statistics stats={[good, neutral, bad]}/>
+		</>
+	)
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
